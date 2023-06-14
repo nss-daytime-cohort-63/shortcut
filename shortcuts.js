d6c84587834d04dd3f12e286b16821179e9b1b39
Devin Kent <dalamcd@gmail.com>
2023-06-14 14:00:43 -0500

    Initial commit

---
+++ shortcuts.js
@@ -0,0 +1,77 @@
+// copy -- ctrl + c
+// paste -- ctrl + v
+// cut -- ctrl + x
+// undo -- ctrl + z
+// redo -- ctrl + shift + z
+// select text -- shift + [arrow/left-click]
+// select word -- 2x left-click
+// select line -- 3x left-click
+ 
+let example = 2
+
+// select line -- ctrl + l
+
+let selectLineExample 
+
+// comment/uncomment line -- ctrl + /
+// comment/uncomment block -- shift + alt + a
+
+let commentExample
+
+// delete line -- ctrl + shift + k
+
+let deleteLineExample
+
+// move selection -- alt + [up/down] arrow
+
+let moveExample1
+let moveExample2
+let moveExample3
+
+for(let i = 0; i < 10; i++) {
+	let forExample
+}
+
+// duplicate selection -- alt + shift + [up/down] arrow
+
+let duplicateExample1
+let duplicateExample2
+let duplicateExample3
+
+// move cursor over word -- ctrl + [left/right] arrow
+
+// test1 test2 "test1" "test1 test2" test(one) test(one, two)
+
+// select similar -- ctrl + d
+
+let explicitVar1 = 1
+let explicitVar2 = 2
+let explicitVar3 = 3
+// ...
+explicitVar1 = 7
+
+// select strictly similar -- ctlr + d (no selection)
+let fgcolor = 1
+let bgColor = 12
+// ...
+fgcolor = 1
+bgColor = 4
+
+// multiple cursor -- ctrl + alt + [up/down] arrow OR ctrl-click
+
+let a = 1
+let b = 1
+let c = 1
+let d = 1
+
+// multi-cursor with move-over-word
+
+let aa = 3
+let bbb = 3
+let cccc = 3
+let ddddd = 3
+// ...
+aa = 3
+bbb = 3
+cccc = 3
+ddddd = 3
